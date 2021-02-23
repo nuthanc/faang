025ecbbc372a96e563bf984954cb8d95b23fafc5
Nuthan Chandra <nuthanc@juniper.net>
2021-02-23 09:07:43 +0530

    Add my js solution

---
+++ Arrays/3/trapRainWater.js
@@ -0,0 +1,25 @@
+var trap = function (height) {
+  let l= (height.length !== 0)? height[0] : 0;
+  let r = 0, rp = 0, val = 0;
+  for (let p=1; p < height.length-1; p++) {
+    if (height[p-1] > l){
+      l = height[p-1]
+    }
+    if (l > height[p]){
+      if (rp <= p){
+          r = 0
+          for (let i=p+1; i<height.length; i++){
+            if( height[i] > r){
+                r = height[i]
+                rp = i
+              }
+            if (r >= l) break;
+          }
+      }
+      if (r > height[p]) val += Math.min(l, r) - height[p];
+    }
+  }
+  return val
+};
+
+console.log(trap((height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1])));
